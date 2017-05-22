e0843bbde948478662f883f4d00eb01738222142
Marcell Hayes <marcell.made@gmail.com>
2017-05-22 14:01:58 -0400

    update fix

--- public/js/apiService.js
+++ public/js/apiService.js
@@ -1,4 +1,34 @@
 angular.module("BookBuddiesMod")
-    .service("apiService", function(){
+    .service("apiService", function($http){
+        
+       this.setBooks = function(search, type) {
+           
+           var promise =
+            $http({
+            method: "GET",
+            url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + type,    
+            params: {
+                key: 'AIzaSyChc5wvGyYkMT2CfK9Tc680QBAqRbbExuA',
+                q: intitle:search
+            }    
+        });   
+        return promise;   
+       };
+    
+        
+        this.setAuthor = function(search, type) {
 
+               var promise =
+                $http({
+                method: "GET",
+                url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + type,    
+                params: {
+                    key: 'AIzaSyChc5wvGyYkMT2CfK9Tc680QBAqRbbExuA',
+                    q: inauthor:search
+                }    
+            });   
+            return promise;   
+           };
+    
+    
     });
