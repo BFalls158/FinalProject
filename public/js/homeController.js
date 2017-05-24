691f74b0bb1202cae66c2b22b7d6c6ea30e445c4
emilyrn <emilynelson@Emilys-MacBook-Pro.local>
2017-05-24 15:38:31 -0400

    modal

--- public/js/homeController.js
+++ public/js/homeController.js
@@ -1,10 +1,51 @@
 angular.module("BookBuddiesMod")
-<<<<<<< HEAD
-    .controller("homeController", function($scope, apiService, dbService){
 
-=======
-    .controller("homeController", function($scope, $http, apiService, dbService){
+    .controller("homeController", function($scope, $http, $modal, apiService, dbService){
+
+
+      $scope.items = ['item1', 'item2', 'item3'];
+
+       $scope.open = function () {
+
+         var modalInstance = $modal.open({
+           templateUrl: 'requestTrade.html',
+           controller: ModalInstanceCtrl,
+           resolve: {
+             items: function () {
+               return $scope.items;
+             }
+           }
+         });
+
+         modalInstance.result.then(function (selectedItem) {
+           $scope.selected = selectedItem;
+         }, function () {
+           $log.info('Modal dismissed at: ' + new Date());
+         });
+       };
+     };
+
+     var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+       $scope.items = items;
+       $scope.selected = {
+         item: $scope.items[0]
+       };
+
+       $scope.ok = function () {
+         $modalInstance.close($scope.selected.item);
+       };
+
+       $scope.cancel = function () {
+         $modalInstance.dismiss('cancel');
+       };
+     };
+
+
+
+
+
 
     	$scope.popularBooks = [];
->>>>>>> 69fca7cf37efbbb45c2c2f37690d562e842cdac8
+
     });
