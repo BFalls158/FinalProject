373ee3faad79af695243e2b3f4fcd3c0b37692b5
Bryan Falls <bfalls1587@gmail.com>
2017-05-24 10:16:59 -0400

    working api call

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ angular.module("BookBuddiesMod", ["ui.bootstrap", "ngAnimate", "ngRoute"])
         .when("/profile", {
             templateUrl: "views/myLibrary.html"
         })
-        .when("/searchresults", {
+        .when("/searchResults", {
             templateUrl: "views/searchResults.html",
             controller: "searchResultsController"
         });
