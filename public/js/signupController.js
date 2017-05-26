7e3f35e54e7031bd9f8a62bd717fcb4c2dbba3b4
emilyrn <emilynelson@Emilys-MacBook-Pro.local>
2017-05-26 15:49:13 -0400

    signup controller

--- public/js/signupController.js
+++ public/js/signupController.js
@@ -1,4 +1,13 @@
 angular.module("BookBuddiesMod")
-    .controller("signupController", function($scope, apiService, dbService){
+    .controller("signupController", function($scope, $http, apiService, dbService, $uibModal, $location){
+
+      $scope.signupToggle = function(size) {
+          var uibmodalInstance = $uibModal.open({
+              animation: $scope.animationsEnabled,
+              templateUrl: 'views/signup.html',
+              controller: 'signupController',
+              windowClass: 'center-modal'
+          })
+      }
 
     });
