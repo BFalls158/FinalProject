e6b59b20ca4b5b4662498fce71d62fe57ae8cd8a
Bryan Falls <bfalls1587@gmail.com>
2017-05-26 22:43:53 -0400

    working login

--- public/js/loginController.js
+++ public/js/loginController.js
@@ -1,14 +1,23 @@
 angular.module("BookBuddiesMod")
-    .controller("loginController", function($scope, apiService, dbService, $location){
+    .controller("loginController", function($scope, $http, apiService, dbService, $location){
 
     	$scope.status = dbService.getStatus();
 
     	$scope.user = dbService.setCurrentUser();
 
 
-    	$scope.logIn = function(user, password) {
-    		dbService.getUserInfo(user).then(function(response) {
+    	$scope.logIn = function(user) {
+    		console.log('function is clicking');
+    		dbService.login(user).then(function(response) {
+    			console.log(response);
+    			if(response === "Success"){
+    				dbService.setCurrentUser(user.username);
+					$scope.loginError = false;
+					$location.path('/home');
+    			} else {
+    				$scope.loginError = true;
+    			}
+    		});
+    	}
 
-    		})
-    	}	
     });
