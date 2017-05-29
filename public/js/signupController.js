4a3c9a32129f6671a10d359c8fab0e4dec573a19
Bryan Falls <bfalls1587@gmail.com>
2017-05-29 14:34:03 -0400

    fixed cancel on signup modal

--- public/js/signupController.js
+++ public/js/signupController.js
@@ -5,9 +5,9 @@ angular.module("BookBuddiesMod")
 
     	$scope.userAvailable = true;
 
-    	// if ($scope.status) {
-    	// 	$location.path('/home');
-    	// }
+    	$scope.ok = function() {
+            $uibModalInstance.close();
+        }
 
 	    $scope.submitForm = function(userInfo) {
 	    	dbService.checkUser(userInfo.username).then(function(response) {
